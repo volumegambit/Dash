5b55d1df79e4b137a08c1a223e75a43e30f20a22
vg <volumegambit@gmail.com>
2026-03-08 21:03:01 +0800

    feat(homepage): add Community Discord CTA section

--- apps/homepage/app/page.tsx
+++ apps/homepage/app/page.tsx
@@ -4,6 +4,7 @@ import { Features } from '@/components/Features'
 import { HowItWorks } from '@/components/HowItWorks'
 import { AppScreenshot } from '@/components/AppScreenshot'
 import { QuickStart } from '@/components/QuickStart'
+import { Community } from '@/components/Community'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <HowItWorks />
       <AppScreenshot />
       <QuickStart />
+      <Community />
     </main>
   )
 }
