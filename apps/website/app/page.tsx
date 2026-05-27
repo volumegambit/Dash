f6ac76bdce77ddb65f1defe0029f6350d3ae2ccb
vg <volumegambit@gmail.com>
2026-05-27 23:01:35 +0800

    refactor(website): drop FinalCTA, MessagingApps, UseCases, WaitlistForm sections

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,13 +1,10 @@
 import { AIProviders } from '@/components/AIProviders';
 import { DeployAndRun } from '@/components/DeployAndRun';
-import { FinalCTA } from '@/components/FinalCTA';
 import { Footer } from '@/components/Footer';
 import { Hero } from '@/components/Hero';
 import { HowItWorks } from '@/components/HowItWorks';
-import { MessagingApps } from '@/components/MessagingApps';
 import { Nav } from '@/components/Nav';
 import { SecureSandbox } from '@/components/SecureSandbox';
-import { UseCases } from '@/components/UseCases';
 
 export default function Home() {
   return (
@@ -17,10 +14,7 @@ export default function Home() {
       <SecureSandbox />
       <DeployAndRun />
       <AIProviders />
-      <MessagingApps />
-      <UseCases />
       <HowItWorks />
-      <FinalCTA />
       <Footer />
     </main>
   );
