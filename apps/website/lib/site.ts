a2a27088ff2cb0c7a73c9b8b5d18e7af5b989c2b
vg <volumegambit@gmail.com>
2026-09-19 22:16:35 +0800

    fix(website): link to docs.dashsquad.ai

--- apps/website/lib/site.ts
+++ apps/website/lib/site.ts
@@ -1,3 +1,3 @@
 export const DOWNLOAD_URL = 'https://github.com/volumegambit/Dash/releases/latest';
-export const DOCS_URL = 'https://dash-aa8db5b5.mintlify.app';
+export const DOCS_URL = 'https://docs.dashsquad.ai';
 export const REPO_URL = 'https://github.com/volumegambit/Dash';
